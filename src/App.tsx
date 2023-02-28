ce7d26ea1c4926aa725946826a44e88cde4b8b50
wenyuan <xuwenyuan2016@163.com>
2023-02-28 16:35:09 +0800

    feat(homepage): side-menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Row, Col } from "antd";
 import styles from "./App.module.css";
 import { Header, Footer} from "./components";
 
@@ -6,6 +7,17 @@ function App() {
   return (
     <div className={styles.App}>
       <Header/>
+      {/* 页面内容 content */}
+      <div className={styles["page-content"]}>
+        <Row style={{marginTop: 20}}>
+          <Col span={6}>
+            <div style={{backgroundColor: "red"}}>多重菜单</div>
+          </Col>
+          <Col span={18}>
+            <div style={{backgroundColor: "blue"}}>走马灯</div>
+          </Col>
+        </Row>
+      </div>
       <Footer/>
     </div>
   );
