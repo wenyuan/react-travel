5a769e3cc01fd632df48358e8bfad388a2cf0934
wenyuan <xuwenyuan2016@163.com>
2023-02-28 16:54:19 +0800

    feat(homepage): carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Row, Col } from "antd";
 import styles from "./App.module.css";
-import { Header, Footer} from "./components";
+import { Header, Footer, SideMenu, Carousel} from "./components";
 
 function App() {
   return (
@@ -11,10 +11,10 @@ function App() {
       <div className={styles["page-content"]}>
         <Row style={{marginTop: 20}}>
           <Col span={6}>
-            <div style={{backgroundColor: "red"}}>多重菜单</div>
+            <SideMenu />
           </Col>
           <Col span={18}>
-            <div style={{backgroundColor: "blue"}}>走马灯</div>
+            <Carousel />
           </Col>
         </Row>
       </div>
